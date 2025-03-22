28048a8fcf7438ee72d9776c56e3a3fbdfe5e21f
F1veStar3 <myklashv@gmail.com>
2025-03-22 17:51:56 +0200

    test cicd

--- core/urls.py
+++ core/urls.py
@@ -22,7 +22,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Your API(tete)",
+        title="Your API",
         default_version='v1',
         description="Your API description",
         terms_of_service="https://www.google.com/policies/terms/",
